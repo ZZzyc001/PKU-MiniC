a827f6f37e71ce88b5e0eb109855bdc92e6bf6ae
ZZzyc001 <zhuyuechen01@gmail.com>
2023-12-15 17:13:18 +0800

    add config

--- src/ast.h
+++ src/ast.h
@@ -4,43 +4,43 @@
 
 // 所有 AST 的基类
 class BaseAST {
- public:
-  virtual ~BaseAST() = default;
+public:
+    virtual ~BaseAST() = default;
 
-  virtual void Dump() const = 0;
+    virtual void Dump() const = 0;
 };
 
 // CompUnit 是 BaseAST
 class CompUnitAST : public BaseAST {
- public:
-  std::unique_ptr<BaseAST> func_def;
+public:
+    std::unique_ptr<BaseAST> func_def;
 
-  void Dump() const override {
-    std::cout << "CompUnitAST { ";
-    func_def->Dump();
-    std::cout << " }";
-  }
+    void Dump() const override {
+        std::cout << "CompUnitAST { ";
+        func_def->Dump();
+        std::cout << " }";
+    }
 };
 
 // FuncDef 也是 BaseAST
 class FuncDefAST : public BaseAST {
- public:
-  std::unique_ptr<BaseAST> func_type;
-  std::string ident;
-  std::unique_ptr<BaseAST> block;
+public:
+    std::unique_ptr<BaseAST> func_type;
+    std::string              ident;
+    std::unique_ptr<BaseAST> block;
 
-  void Dump() const override {
-    std::cout << "FuncDefAST { ";
-    func_type->Dump();
-    std::cout << ", " << ident << ", ";
-    block->Dump();
-    std::cout << " }";
-  }
+    void Dump() const override {
+        std::cout << "FuncDefAST { ";
+        func_type->Dump();
+        std::cout << ", " << ident << ", ";
+        block->Dump();
+        std::cout << " }";
+    }
 };
 
 // FuncType
 class FuncTypeAST : public BaseAST {
-    public:
+public:
     void Dump() const override {
         std::cout << "FuncTypeAST { ";
         std::cout << "int";
@@ -55,7 +55,7 @@ public:
 
     void Dump() const override {
         std::cout << "BlockAST { ";
-        stmt -> Dump();
+        stmt->Dump();
         std::cout << " }";
     }
 };
